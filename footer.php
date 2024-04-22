d47850ac80080a86029200ecedd435692d4c7028
Amelia <u1345994@utah.edu>
2024-04-22 00:22:59 -0600

    Added .php files to create Wordpress theme

---
+++ footer.php
@@ -0,0 +1,48 @@
+<body>
+	<div id="site-wrap">
+		<div>
+		</div>
+        <div class="big-logo">
+            <ul class="title">
+                <li><a class="active" href="index.html"><img src="Assets/logoo@2x.png" class="logo" alt="logo"></a></li>
+                <li><h2>Something Specific?</h2></li>
+				<li class="contact-form"><a href="contact.html">Contact Form Here</a></li>
+            </ul>
+        </div>
+		<div class="start">
+            <ul class="title">
+                <li><h1>Where would you like to start?</h2></li>
+            </ul>
+        </div>
+		<div class="about-art">
+			<ul class="nav">
+				<li><a href="about.html">About</a></li>
+                <li><a href="artwork.html">Artwork</a></li>
+			</ul>
+		</div>
+		<div class="shop-phys">
+			<ul class="nav">
+				<li><a href="physical.html">Physical</a></li>
+                <li><a href="shop.html">Shop</a></li>
+			</ul>
+		</div>
+		<div class="res-dig">
+			<ul class="nav">
+				<li><a href="resume.html">Resume</a></li>
+				<li><a href="Digital.html">Digital</a></li>				
+			</ul>
+		</div>
+		<div class=line></div>
+		<div class="menu-phone">
+			<ul class="nav-phone">
+				<li><a href="about.html"><img src="Assets/first.png" class="first" alt="first"></a></li>
+				<li><a href="resume.html"><img src="Assets/second.png" class="second" alt="second"></a></li>
+				<li><a href="Digital.html"><img src="Assets/third.png" class="third" alt="third"></a></li>
+				<li><a href="physical.html"><img src="Assets/fourth.png" class="fourth" alt="fourth"></a></li>
+				<li><a href="contact.html"><img src="Assets/fifth.png" class="fifth" alt="fifth"></a></li>
+			</ul>
+		</div>
+	</div>
+<?php wp_footer();?>
+</body>
+</html>
