c32d6e73a6ebee3e8595cbcf673bf8bbb6f322cc
Amelia <u1345994@utah.edu>
2024-10-11 21:16:31 -0600

    removed some excess files

--- header.php
+++
@@ -1,8 +0,0 @@
-<!doctype html>
-<html>
-<head>
-	<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1">
-	<title>index</title>
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css" type="text/css" media="all" />
-<?php wp_head();?>
-</head>
